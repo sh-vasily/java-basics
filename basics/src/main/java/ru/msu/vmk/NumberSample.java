601741b1d6d6615b09e3b8d9ad51411362e1f3a3
Василий Щербаков <basil-scherbakov@ya.ru>
2024-02-29 19:30:26 +0300

    add array

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -6,6 +6,7 @@ public class NumberSample {
     public static BigDecimal[] split(BigDecimal amount, int n) {
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
+        var array = new BigDecimal[]{};
         return null;
     }
 }

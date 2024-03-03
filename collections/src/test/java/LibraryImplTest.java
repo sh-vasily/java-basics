43f3c7c5ea68b1eec0c95b508e5df3862cc37507
Василий Щербаков <basil-scherbakov@ya.ru>
2024-03-03 21:20:04 +0300

    add collections task

---
+++ collections/src/test/java/LibraryImplTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import ru.msu.vmk.Book;
+import ru.msu.vmk.LibraryImpl;
+import org.junit.jupiter.api.Assertions;
+
+public class LibraryImplTest {
+    @Test
+    public void addBookTest(){
+        var library = new LibraryImpl();
+        library.addNewBook(new Book("book"));
+        var availableBooks = library.findAvailableBooks();
+
+        Assertions.assertEquals(1, availableBooks.size());
+        Assertions.assertEquals("book", availableBooks.get(0).getTitle());
+    }
+
+    @Test
+    public void borrowAndReturnBookTest(){
+        var library = new LibraryImpl();
+        var book = new Book("book");
+
+        library.addNewBook(book);
+
+        var availableBooks = library.findAvailableBooks();
+        Assertions.assertEquals(1, availableBooks.size());
+        Assertions.assertEquals("book", availableBooks.get(0).getTitle());
+
+        library.borrowBook(book, "student");
+
+        availableBooks = library.findAvailableBooks();
+        Assertions.assertEquals(0, availableBooks.size());
+
+        library.returnBook(book, "student");
+
+        availableBooks = library.findAvailableBooks();
+        Assertions.assertEquals(1, availableBooks.size());
+        Assertions.assertEquals("book", availableBooks.get(0).getTitle());
+    }
+}

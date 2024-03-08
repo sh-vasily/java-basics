e9173f75688777a2372c4ec65734719763275b0c
OlgaMihail <ombazanova@gmail.com>
2024-03-08 08:35:22 +0300

    homework 1

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -1,11 +1,25 @@
 package ru.msu.vmk;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class NumberSample {
+
     public static BigDecimal[] split(BigDecimal amount, int n) {
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
-        return null;
+        var array = new BigDecimal[n];
+        //var k = amount.scale();
+        BigDecimal sum = BigDecimal.valueOf(0.0);
+        //var a = amount.divide(BigDecimal.valueOf(n), k,6);
+        var a = amount.divide(BigDecimal.valueOf(n));
+
+        for(int i = 1; i < n; ++i){
+            array[i] = a;
+            sum = sum.add(array[i]);
+        }
+        array[0] = amount.subtract(sum);
+
+        return array;
     }
 }

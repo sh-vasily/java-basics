429aa10be2f9e6fba462bc87494cb59da5df43f2
arthur_kuspakov <arthur.60284@gmail.com>
2024-03-13 01:04:22 +0300

    Create test class QuantityTest.java

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -3,12 +3,23 @@ package ru.msu.vmk;
 import java.math.BigDecimal;
 
 public class NumberSample {
+
     public static BigDecimal[] split(BigDecimal amount, int n) {
         var array = new BigDecimal[n];
         double temp = amount.doubleValue();
+        int previousSize = n;
         for (int i = 0; n > 0; i++, n--) {
             double size = temp / n;
-            array[i] = BigDecimal.valueOf(size);
+            if(previousSize > 2) {
+                if (i == 0) {
+                    double ost = temp % n;
+                    array[i] = BigDecimal.valueOf((int) size).add(BigDecimal.valueOf(ost));
+                }else {
+                    array[i] = BigDecimal.valueOf((int)size);
+                }
+            } else {
+                    array[i] = BigDecimal.valueOf(size);
+            }
             temp -= size;
         }
         return array;

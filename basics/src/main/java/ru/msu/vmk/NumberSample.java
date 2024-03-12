2107e4afe48080e8da0462a13ca1cc6fb23f2b09
kuspakov <kuspakov@gmail.com>
2024-03-13 00:06:55 +0300

    Update NumberSample.java

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -6,6 +6,13 @@ public class NumberSample {
     public static BigDecimal[] split(BigDecimal amount, int n) {
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
-        return null;
+        var array = new BigDecimal[n];
+        double temp = amount.doubleValue();
+        for (int i = 0; n > 0; i++, n--) {
+            double size = temp / n;
+            array[i] = BigDecimal.valueOf(size);
+            temp -= size;
+        }
+        return array;
     }
 }

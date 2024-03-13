4711332f61b07c790162c631440e62dd28da3473
storyia <storyia90@gmail.com>
2024-03-14 00:48:10 +0100

    Task 1

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -3,9 +3,26 @@ package ru.msu.vmk;
 import java.math.BigDecimal;
 
 public class NumberSample {
+    public static void main(String[] args) {
+        // вызов split - проверка работы
+        BigDecimal amount = new BigDecimal("100");
+        int n = 3;
+        BigDecimal[] parts = split(amount, n);
+        for (BigDecimal part : parts) {
+            System.out.println(part);
+        }
+
+    }
     public static BigDecimal[] split(BigDecimal amount, int n) {
+        var result = amount.divide(new BigDecimal(n));
+        var reminder = amount.remainder(new BigDecimal(n));
+        var array = new BigDecimal[n];
+        for (int i=0; i < n; i++) {
+            array[i] = result;
+        }
+
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
-        return null;
+        return array;
     }
 }

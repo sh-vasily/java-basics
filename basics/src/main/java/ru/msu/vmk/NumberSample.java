3be7fcf51934c5d44e9dc7d828af913227203fea
kuspakov <kuspakov@gmail.com>
2024-03-14 21:50:10 +0300

    Update NumberSample.java

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -3,15 +3,22 @@ package ru.msu.vmk;
 import java.math.BigDecimal;
 
 public class NumberSample {
+
     public static BigDecimal[] split(BigDecimal amount, int n) {
-        // разделить amount на n частей
-        // если остается остаток, прибавить его к первому числу
         var array = new BigDecimal[n];
         double temp = amount.doubleValue();
+        int previousSize = n;
         for (int i = 0; n > 0; i++, n--) {
             double size = temp / n;
-            array[i] = BigDecimal.valueOf(size);
+            if (previousSize > 2) {
+                array[i] = BigDecimal.valueOf((int) size);
+            } else {
+                array[i] = BigDecimal.valueOf(size);
+            }
             temp -= size;
+        }
+        if (previousSize > 2) {
+            array[0] = BigDecimal.valueOf((int) (amount.doubleValue() / previousSize)).add(BigDecimal.valueOf(amount.doubleValue() % previousSize));
         }
         return array;
     }

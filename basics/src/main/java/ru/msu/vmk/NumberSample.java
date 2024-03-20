b03f60a230ba8ffa8ddef6cce3739b528aba5a0a
B14D3E33 <bordingosha@gmail.com>
2024-03-20 16:32:58 +0300

    homework_1 with remainder

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -2,18 +2,19 @@ package ru.msu.vmk;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
 
 public class NumberSample {
     public static BigDecimal[] split(BigDecimal amount, int n) {
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
+        BigDecimal dividedEqualAmount = amount.divide(new BigDecimal(n), 2, RoundingMode.DOWN)
+                                        .stripTrailingZeros().movePointRight(1).movePointLeft(1);
+        BigDecimal remainder = amount.subtract(dividedEqualAmount.multiply(new BigDecimal(n)));
+
         BigDecimal[] result = new BigDecimal[n];
-        BigDecimal splittedEqualAmount = amount.divide(new BigDecimal(n), 2, RoundingMode.DOWN)
-        .stripTrailingZeros().movePointRight(1).movePointLeft(1);
-        
-        for (int i = 0; i < n; i++) {
-            result[i] = splittedEqualAmount;
-        }
+        Arrays.fill(result, dividedEqualAmount);
+        result[0].add(remainder).stripTrailingZeros().movePointRight(1).movePointLeft(1);
 
         return result;
     }

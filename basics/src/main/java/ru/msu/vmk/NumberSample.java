68739480c0aabe39857f1905d9cf6168b7faeb4a
Alexei Sosenkov <leha.sosenkov@gmail.com>
2024-03-20 09:55:28 +0300

    Task-1 is completed

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -2,11 +2,14 @@ package ru.msu.vmk;
 
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class NumberSample {
 
     public static BigDecimal[] split(BigDecimal amount, int n) {
         var result = amount.divide(new BigDecimal(n));
-        return new BigDecimal[]{result, result};
+        BigDecimal[] resArray = new BigDecimal[n];
+        Arrays.fill(resArray, result);
+        return resArray;
     }
 }

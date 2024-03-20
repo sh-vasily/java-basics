780233d42335608cf3ac4308482989c8c62e5774
Alexei Sosenkov <leha.sosenkov@gmail.com>
2024-03-20 15:34:12 +0300

    [task-3] Completed, tests has been passed

--- collections/src/main/java/ru/msu/vmk/Book.java
+++ collections/src/main/java/ru/msu/vmk/Book.java
@@ -1,5 +1,7 @@
 package ru.msu.vmk;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
 
@@ -9,5 +11,17 @@ public class Book {
 
     public String getTitle(){
         return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(getTitle(), book.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle());
     }
 }

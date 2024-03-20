30550ec87d0be68ff446f9dc67dd6850a1f7e826
B14D3E33 <bordingosha@gmail.com>
2024-03-20 15:32:08 +0300

    homework_1

--- basics/src/main/java/ru/msu/vmk/NumberSample.java
+++ basics/src/main/java/ru/msu/vmk/NumberSample.java
@@ -1,11 +1,20 @@
 package ru.msu.vmk;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class NumberSample {
     public static BigDecimal[] split(BigDecimal amount, int n) {
         // разделить amount на n частей
         // если остается остаток, прибавить его к первому числу
-        return null;
+        BigDecimal[] result = new BigDecimal[n];
+        BigDecimal splittedEqualAmount = amount.divide(new BigDecimal(n), 2, RoundingMode.DOWN)
+        .stripTrailingZeros().movePointRight(1).movePointLeft(1);
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = splittedEqualAmount;
+        }
+
+        return result;
     }
 }
